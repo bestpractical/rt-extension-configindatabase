634a1bc72c08594ffed4808ed1ebf79409fd53d2
Shawn M Moore <shawn@bestpractical.com>
2017-08-02 22:12:10 +0000

    Scaffolding

---
+++ lib/RT/Extension/ConfigInDatabase.pm
@@ -0,0 +1,61 @@
+package RT::Extension::ConfigInDatabase;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+RT-Extension-ConfigInDatabase - update RT config via admin UI
+
+=head1 INSTALLATION
+
+=over
+
+=item perl Makefile.PL
+
+=item make
+
+=item make install
+
+This step may require root permissions.
+
+=item Edit your /opt/rt4/etc/RT_SiteConfig.pm
+
+Add this line:
+
+    Plugin( "RT::Extension::ConfigInDatabase" );
+
+=item Clear your mason cache
+
+    rm -rf /opt/rt4/var/mason_data/obj
+
+=item Restart your webserver
+
+=back
+
+=head1 AUTHOR
+
+Best Practical Solutions, LLC E<lt>modules@bestpractical.comE<gt>
+
+=head1 BUGS
+
+All bugs should be reported via email to
+
+    L<bug-RT-Extension-ConfigInDatabase@rt.cpan.org|mailto:bug-RT-Extension-ConfigInDatabase@rt.cpan.org>
+
+or via the web at
+
+    L<rt.cpan.org|http://rt.cpan.org/Public/Dist/Display.html?Name=RT-Extension-ConfigInDatabase>.
+
+=head1 COPYRIGHT
+
+This extension is Copyright (C) 2017 Best Practical Solutions, LLC.
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 2, June 1991
+
+=cut
+
+1;

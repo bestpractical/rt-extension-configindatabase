7d997ab30626a66698372553d62c37e81f62dafe
Shawn M Moore <shawn@bestpractical.com>
2017-08-02 22:15:49 +0000

    Load and apply all config from the database

--- lib/RT/Extension/ConfigInDatabase.pm
+++ lib/RT/Extension/ConfigInDatabase.pm
@@ -1,8 +1,44 @@
 package RT::Extension::ConfigInDatabase;
 use strict;
 use warnings;
+use RT::DatabaseSetting;
+use RT::DatabaseSettings;
 
 our $VERSION = '0.01';
+
+__PACKAGE__->LoadConfigFromDatabase();
+
+sub LoadConfigFromDatabase {
+    my $class = shift;
+
+    my $settings = RT::DatabaseSettings->new(RT->SystemUser);
+    $settings->UnLimit;
+
+    while (my $setting = $settings->Next) {
+        my $name = $setting->Name;
+        my $value = $setting->Content;
+
+        use Data::Dumper;
+        local $Data::Dumper::Terse = 1;
+        RT->Logger->debug("from database: Set('$name', ".Dumper($value).");");
+
+        # are we inadvertantly overriding RT_SiteConfig.pm?
+        my $meta = RT->Config->Meta($name);
+        my %source = %{ $meta->{'Source'} };
+        if ($source{'SiteConfig'} && $source{'File'} ne 'database') {
+            RT->Logger->warning("Change of config option '$name' at $source{File} line $source{Line} has been overridden by the config setting from the database. Please remove it from $source{File} or from the database to avoid confusion.");
+        }
+
+        RT->Config->SetFromConfig(
+            Option     => \$name,
+            Value      => [$value],
+            Package    => 'N/A',
+            File       => 'database',
+            Line       => 'N/A',
+            SiteConfig => 1,
+        );
+    }
+}
 
 =head1 NAME
 
